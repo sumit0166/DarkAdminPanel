fd036c1242259e6ad8d50a0aa8334dd87841fc2c
Sumit Sakpal <sumitsakpal0183@gmail.com>
2024-03-30 12:55:56 +0530

    created settings page

--- src/frontend/Body/Inventory/SelectBox.js
+++ src/frontend/Body/Inventory/SelectBox.js
@@ -3,38 +3,77 @@ import { useEffect, useRef, useState } from 'react';
 import { ArrowDown2 } from 'iconsax-react';
 // import React, { useState } from 'react';
 
-function SelectBox({isActive, options, name, ICON, setActiveList, activeList }) {
-    const [selected,setSelected] = useState(null);
-    const [isExtended,setIsExtended] = useState(true)
+function SelectBox({ isActive, options, name, ICON, setActiveList, activeList }) {
+    const [selected, setSelected] = useState(null);
+    const [isExtended, setIsExtended] = useState(true)
     const selectBoxCont = useRef()
     const svg = useRef()
 
-    useEffect(() => {setSelected(activeList) },[activeList])
+    useEffect(() => { setSelected(activeList) }, [activeList])
 
-    useEffect(() => {setSelected(options[0])},[])
-    
-    
-    const extendSelect = () => {
-        setIsExtended(!isExtended);
-        if (isExtended) {
-            selectBoxCont.current.style.height = "fit-content"; 
-            selectBoxCont.current.style.padding = "5px 10px";
-            selectBoxCont.current.style.paddingBottom = "10px";
-            selectBoxCont.current.style.opacity = "1";
-            svg.current.style.rotate = "180deg";
-        } else {
-            selectBoxCont.current.style.padding = "0 10px";
-            selectBoxCont.current.style.opacity = "0";
-            selectBoxCont.current.style.height = "0px"; 
-            svg.current.style.rotate = "0deg";
-        }
+    useEffect(() => { setSelected(options[0]) }, [])
+
+    const openOptions = () => {
+        selectBoxCont.current.style.height = "fit-content";
+        selectBoxCont.current.style.padding = "10px 10px";
+        selectBoxCont.current.style.paddingBottom = "10px";
+        selectBoxCont.current.style.opacity = "1";
+        svg.current.style.rotate = "180deg";
     }
+
+    const closeOptions = () => {
+        selectBoxCont.current.style.height = "0px";
+        selectBoxCont.current.style.padding = "0 10px";
+        selectBoxCont.current.style.opacity = "0";
+        svg.current.style.rotate = "0deg";
+        setIsExtended(!isExtended);
+    }
+
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            if (event.keyCode === 27) { // 27 is the key code for ESC key
+                closeOptions();
+            }
+        };
+
+        document.body.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            document.body.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
+
+    const  handleEsc = event => {
+        console.log(event.keyCode);
+        if (event.keyCode === 27) {
+          event.preventDefault();
+          alert('You hit esc!');
+        }
+      }
+
+
+    const extendSelect = () => {
+        openOptions();
+        // console.log(isExtended);
+        // setIsExtended(!isExtended);
+        // if (isExtended) {
+        //     openOptions();
+        // } else {
+        //     selectBoxCont.current.style.height = "0px";
+        //     selectBoxCont.current.style.padding = "0 10px";
+        //     selectBoxCont.current.style.opacity = "0";
+        //     svg.current.style.rotate = "0deg";
+        // }
+    }
+
+
+
     const handleSelected = (e) => {
         setSelected(e);
-        if(setActiveList){
+        if (setActiveList) {
             setActiveList(e);
         }
-        selectBoxCont.current.style.height = "0px"; 
+        selectBoxCont.current.style.height = "0px";
         selectBoxCont.current.style.padding = "0 10px";
         selectBoxCont.current.style.opacity = "0";
         svg.current.style.rotate = "0deg";
@@ -42,16 +81,16 @@ function SelectBox({isActive, options, name, ICON, setActiveList, activeList }) 
     }
 
     return (
-        <div className={ isActive ? "SelectBox SelectBox-active" :"SelectBox"} >
+        <div className={isActive ? "SelectBox SelectBox-active" : "SelectBox"} >
             <div className="selectbox-main" onClick={extendSelect} >
-                <ICON size="22" variant="Bold"/>
-                { name && <span>{name}:</span>}
+                <ICON size="22" variant="Bold" />
+                {name && <span>{name}:</span>}
                 <span className="sortSelected" >{selected == "None" ? "" : selected}</span>
-                <ArrowDown2 size="20" variant="Bold" ref={svg}/>
+                <ArrowDown2 size="20" variant="Bold" ref={svg} />
             </div>
-            <div className="selectbox-opt-cont" ref={selectBoxCont}>
+            <div className="selectbox-opt-cont" ref={selectBoxCont} >
                 {
-                    options.map( (item) => <span onClick={()=> handleSelected(item)}>{item}</span>)
+                    options.map((item) => <span onClick={() => handleSelected(item)}>{item}</span>)
                 }
             </div>
         </div>
--- src/frontend/Body/Settings/Settings.js
+++ src/frontend/Body/Settings/Settings.js
@@ -1,19 +1,68 @@
 import './Settings.css';
 
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import { useAutoAnimate } from '@formkit/auto-animate/react';
 // const response = await fetch('/config.json');
 // const config = await response.json();
+import { SecurityUser, ColorSwatch } from 'iconsax-react';
+import { useState } from 'react';
 
+const MenuButton = ({Icon, name, path, setActivePage}) => {
+  const [isHover, setHover] = useState(false);
+  // const navigate = useNavigate();
+
+  const handleClick = () => {
+    setActivePage(name);
+    // navigate('/Settings/' + name, { replace: false })
+  }
+
+  return (
+    <div className="MenuButton"
+      onMouseEnter={()=> setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      onClick={handleClick}
+    >
+      {Icon && <Icon size="24" color="var(--primary)" variant={isHover ? "Bulk" :"Linear"} />}
+      <h4>{name}</h4>
+    </div>
+  );
+}
+
+
+const IAM = () => {
+  return(
+    <div className="IAM">
+      <center><h1>User Management</h1></center>
+    </div>
+  )
+}
+
+
+const Personalise = () => {
+  return(
+    <div className="Personalise">
+      <center><h1>Personalise</h1></center>
+    </div>
+  )
+}
 
 
 function Settings() {
+  const [activePage, setActivePage] = useState('');
+  const animateParent = useAutoAnimate();
 
   return (
-    <div className="Settings" style={{height: "100vh",  background: '#DFE7F0'}}>
+    <div className="Settings" >
+      <div className="leftMenu">
+        <h4 style={{padding:'10px', marginBottom:'1%', fontSize:'14px', color:'var(--pri-color)'}}>Options</h4>
+        <MenuButton name="Personalize" Icon={ColorSwatch} setActivePage={setActivePage} isActive={activePage === "Personalize"}/>
+        <MenuButton name="User Management" Icon={SecurityUser} setActivePage={setActivePage} isActive={activePage === "IAM"}/>
 
-      <center><h1><strong>Settings</strong></h1></center>
-
+      </div>
+      <div className="rightPageCont" ref={animateParent}>
+        {activePage === "Personalize" && <Personalise />}
+        {activePage === "User Management" && <IAM />}
+      </div>
     </div>
   );
 }
